b920256a01cb5131ec837ac38df02bd0fb48b155
rk <it@indiba.in>
2017-02-26 14:55:55 +0530

    add message box

--- Example.java
+++ Example.java
@@ -1,25 +1,37 @@
 import java.awt.*;  
 import java.awt.event.*;  
 import javax.swing.*;
+import java.sql.*;
 public class Example implements ActionListener {
-	JFrame frame;
-	JButton btnLogin, btnCancel;
-	JTextField txtUsername, txtPassword;
+	JFrame frame,frame2;
+	JButton btnLogin, btnCancel, btnSubmit, btnClear;
+	JTextField txtUsername, txtPassword,txtSchool_name, txtSchool_principal;
 	Example() {
 		frame = new JFrame();
+		frame2 = new JFrame();
 
 		frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING)); 
+		frame2.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING)); 
 
 		btnLogin = new JButton("Login");
 		btnCancel = new JButton("Cancel");
 
+		btnSubmit = new JButton("Submit");
+		btnClear = new JButton("Cancel");
+
 	  txtUsername = new JTextField("Username");  
 	  txtPassword = new JTextField("Password");  
-	  
+	  txtSchool_principal = new JTextField("s");  
+	  txtSchool_name = new JTextField("");
+	 
 	  txtUsername.setBounds(10,10,210,30);  
 	  txtPassword.setBounds(10,50,210,30);  
+	  txtSchool_principal.setBounds(10,10,210,30);  
+	  txtSchool_name.setBounds(10,50,210,30);  
 	  frame.add(txtUsername);
 	  frame.add(txtPassword);
+	  frame2.add(txtSchool_principal);
+	  frame2.add(txtSchool_name);
 
 	  btnLogin.setBounds(10,90,100,30);
 		btnCancel.setBounds(120,90,100,30);
@@ -29,20 +41,36 @@ public class Example implements ActionListener {
 		frame.setLayout(null);
 		frame.setVisible(true);
 
+		btnSubmit.setBounds(50,130,100,30);
+		btnClear.setBounds(180,130,100,30);
+		frame2.add(btnSubmit);
+		frame2.add(btnClear);
+		frame2.setSize(1200,800);
+		frame2.setLayout(null);
+		
+
 		btnLogin.addActionListener(this);  
     btnCancel.addActionListener(this);  
+	  btnSubmit.addActionListener(this);  
+    btnClear.addActionListener(this);
+
+
 	}
 		
 
 	public void actionPerformed(ActionEvent e) {  
     
     if(e.getSource() == btnLogin){  
+    	JOptionPane.showMessageDialog(frame2,"Hello, Welcome to Javatpoint.");
       String username = txtUsername.getText();
       String password = txtPassword.getText();
 
-      JDBCTest db = new JDBCTest();
-	    db.connect(username, password);
+   
+      	JDBCTest db = new JDBCTest();
+	    	db.connect(username, password);
 
+	    frame2.setVisible(true);
+	    frame.setVisible(false);
     }else if(e.getSource() == btnCancel){  
       txtUsername.setText("");
       txtPassword.setText("");
--- JDBCTest.java
+++ JDBCTest.java
@@ -8,11 +8,12 @@ class JDBCTest {
     String url = "jdbc:mysql://localhost:3306/student_attend";
     String db_user = "root";
     String db_password = "root";
+    Connection con;
+    Statement stmt;
     try {
       Class.forName("com.mysql.jdbc.Driver");
-      Connection con;
       con = DriverManager.getConnection(url, db_user, db_password);
-      Statement stmt = con.createStatement();  
+      stmt = con.createStatement();
       ResultSet rs = stmt.executeQuery("select * from users");
 
       while(rs.next()) {
@@ -26,7 +27,7 @@ class JDBCTest {
       }
       con.close();
     } catch (Exception e) {
-      e.printStackTrace(); 
+      e.printStackTrace();
     }
   }
 }
